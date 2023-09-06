3eb5558f03c3f7cbda5571d6932b90070629cf5b
Sudhanshu <110216470+Sudhanshu8257@users.noreply.github.com>
2023-09-06 10:49:30 +0530

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:[
+            'xomwqbxggwtzvtaqgtfq.supabase.co'
+        ]
+    }
+}
+
+module.exports = nextConfig
